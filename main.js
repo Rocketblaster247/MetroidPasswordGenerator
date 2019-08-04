bc77d3c6b81f0fccd08480364fbc024062fff41f
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-08-04 15:32:49 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var abc = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?".toString();
+abc[0xFF] = "-";
+var hex = 0x00000000000000000000000000000000;
+var Pass2Hex = function (pass) {
+  for (var i = 0; i < pass.length; i ++) {
+    console.log();
+  }
+};
